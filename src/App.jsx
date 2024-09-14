8c008fb1003f2649776850f3b17b42397224a4a0
Takitok2011 <bekzodeoke@gmail.com>
2024-09-14 21:49:17 +0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Title from "./components/Title/title";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <div className="app">
         <Hero />
         <About />
+        <Title title={"my "} title2={"Services"} />
       </div>
     </>
   );
